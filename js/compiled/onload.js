fc29b1035677e4f9c9c1f67ac8b0ef7a7655d792
Marek Nowak <mareknowakk@gmail.com>
2012-11-12 01:01:30 +0100

    Displays the track's title and artist on load.

--- js/compiled/onload.js
+++ js/compiled/onload.js
@@ -2,7 +2,16 @@
 (function() {
 
   window.onload = function() {
-    return console.log('This is a test of CoffeeScript.');
+    GL.sp = getSpotifyApi(1);
+    GL.models = GL.sp.require('sp://import/scripts/api/models');
+    GL.player = GL.models.player;
+    GL.elements = {};
+    GL.elements.titleElement = document.getElementById('title');
+    GL.elements.artistElement = document.getElementById('artist');
+    if (GL.player.track) {
+      GL.elements.titleElement.innerHTML = GL.player.track.name;
+      return GL.elements.artistElement.innerHTML = GL.player.track.artists[0].toString();
+    }
   };
 
 }).call(this);
--- js/src/onload.coffee
+++ js/src/onload.coffee
@@ -1,2 +1,13 @@
 window.onload = ->
-  console.log 'This is a test of CoffeeScript.'
+  GL.sp = getSpotifyApi 1
+  GL.models = GL.sp.require 'sp://import/scripts/api/models'
+  GL.player = GL.models.player
+
+  GL.elements = {}
+
+  GL.elements.titleElement = document.getElementById 'title'
+  GL.elements.artistElement = document.getElementById 'artist'
+
+  if GL.player.track
+    GL.elements.titleElement.innerHTML = GL.player.track.name
+    GL.elements.artistElement.innerHTML = GL.player.track.artists[0].toString()
